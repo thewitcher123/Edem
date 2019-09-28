5c54235c7afb5a8d3b0ed0a18226e988800c62f6
ivashkevichdn <ivashkevichdn@gmail.com>
2019-09-29 01:55:49 +0300

    Начальная платформа FrontEnd`а.

---
+++ src/frontend/hubcore/components/Hubcore.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../styles/Hubcore.css';
+
+class Hubcore extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hubcore!</h1>
+      </div>
+    );
+  }
+}
+
+export default Hubcore;
--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -1,28 +1,47 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './hubcore/index.js',
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: 'bundel.js'
+    path: path.join(__dirname, 'dist'),
+    filename: 'index_mini.js',
   },
-
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    watchContentBase: true,
+    progress: true,
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }, 
-
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
+        ],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './client/index.html'
-    })
-  ]
-}
+      template: './hubcore/index.html',
+    }),
+  ],
+};
